04ba76f24727e6bed3698b7c6e7e2a109d0d976c
Martin Miselli <tinchotinchocarp@gmail.com>
2023-02-17 17:39:53 -0300

    Mi primer commit

---
+++ js/dulcecalu.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+document.getElementById("calu").addEventListener("click", web);
+
+function web() {
+    window.open("https://www.instagram.com/_calu.distribuidora/")
+}
+
+document.getElementById('pastafrola').addEventListener('click', function() {
+window.location.href = '#arpastafrola';
+});
+
+document.getElementById('alfajores').addEventListener('click', function() {
+window.location.href = '#aralfajores';
+});
+
+document.getElementById('surtidas').addEventListener('click', function() {
+window.location.href = '#arsurtidas';
+});
+
+document.getElementById('pepas').addEventListener('click', function() {
+window.location.href = '#arpepas';
+});
+    
+document.getElementById('chipschocolate').addEventListener('click', function() {
+window.location.href = '#archipschocolate';
+});
+    
+document.getElementById('alfajorcitos').addEventListener('click', function() {
+window.location.href = '#aralfajorcitos';
+});
+
+document.getElementById('galles').addEventListener('click', function() {
+window.location.href = '#argalles';
+});
+    
+document.getElementById('gallesmitimiti').addEventListener('click', function() {
+window.location.href = '#argallesmitimiti';
+});
+    
+document.getElementById('gallesblancas').addEventListener('click', function() {
+window.location.href = '#argallesblancas';
+});
+    
+document.getElementById('bizcochitos').addEventListener('click', function() {
+window.location.href = '#arbizcochitos';
+});
+        
+document.getElementById('panpapa').addEventListener('click', function() {
+window.location.href = '#arpanpapa';
+});
+        
+document.getElementById('talitas').addEventListener('click', function() {
+window.location.href = '#artalitas';
+});
+
+document.getElementById('medialunas').addEventListener('click', function() {
+window.location.href = '#armedialunas';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
